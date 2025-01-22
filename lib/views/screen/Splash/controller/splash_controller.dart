8e22988ce08bfd20d500273fafc0fc5feb093da2
Zaman Sheikh <zaman6545@gmail.com>
2025-01-22 12:39:43 +0600

    app logger

---
+++ lib/views/screen/Splash/controller/splash_controller.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
+
+class SplashController extends GetxController implements GetxService {
+  String title = "Home Screen";
+
+  @override
+  void onInit() {
+    debugPrint("On Init  $title");
+
+    super.onInit();
+  }
+
+  void letsGo() {
+    debugPrint("Lets Go");
+  }
+
+  @override
+  void onReady() {
+    // TODO: implement onReady
+    debugPrint("On onReady  $title");
+    super.onReady();
+  }
+}
